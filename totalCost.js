91b82d87318a94d51f0bc48875d5bc21f97fc7ce
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-02-03 10:00:56 +0600

    how to find total cost with given fixed price

---
+++ totalCost.js
@@ -0,0 +1,14 @@
+const aluPrice= 25;
+const ricePrice= 70;
+const fishPrice= 200;
+
+const aluQuantity = 5;
+const riceQuantity = 3;
+const fishQuantity = 1.5;
+
+const totalAluPrice = aluPrice * aluQuantity;
+console.log(totalAluPrice);
+const totalRicePrice = ricePrice * riceQuantity;
+console.log(totalRicePrice);
+const totalFishPrice = fishPrice * fishQuantity;
+console.log(totalFishPrice);
