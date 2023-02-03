59ba5d2d01e94f9005f79dcc59343d41910b92fd
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2023-02-03 09:48:47 +0600

    replace a number by a word

---
+++ replace.js
@@ -0,0 +1,19 @@
+function replaceNum (){
+    for(var i = 1 ; i <= 50; i++){
+        // console.log(i);
+        if((i % 3 ===0) && (i % 5 === 0)){
+            console.log("foobar");
+        }
+        else if(i % 3 ===0){
+            console.log("foo");
+        }
+        else if(i % 5 === 0){
+            console.log("bar");
+        }
+        else{
+            console.log(i);
+        }
+    }
+}
+
+replaceNum ();
